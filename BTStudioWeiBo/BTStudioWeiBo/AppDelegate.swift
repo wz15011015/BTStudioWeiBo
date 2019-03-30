49b0f6cb5999190e4de8ea090f3c64571144e572
Brian Wang <wzqsyk@126.com>
2019-03-30 10:54:45 +0800

    项目目录结构的创建.

--- BTStudioWeiBo/BTStudioWeiBo/AppDelegate.swift
+++ BTStudioWeiBo/BTStudioWeiBo/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        
+        window?.rootViewController = BWMainViewController()
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ BTStudioWeiBo/BTStudioWeiBo/Class/View(View_Controller)/Discovery/BWDiscoveryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BWDiscoveryViewController.swift
+//  BTStudioWeiBo
+//
+//  Created by hadlinks on 2019/3/30.
+//  Copyright © 2019 BTStudio. All rights reserved.
+//
+
+import UIKit
+
+class BWDiscoveryViewController: BWBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
