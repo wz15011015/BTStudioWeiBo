6125840992a07747211f5ac649a38a5a7196907a
Brian Wang <wzqsyk@126.com>
2019-03-30 11:10:25 +0800

    添加CZAdditions, 通过桥接文件WeiBo_Bridge使用OC类.

---
+++ BTStudioWeiBo/BTStudioWeiBo/WeiBo_Bridge.h
@@ -0,0 +1,14 @@
+//
+//  WeiBo_Bridge.h
+//  BTStudioWeiBo
+//
+//  Created by hadlinks on 2019/3/30.
+//  Copyright © 2019 BTStudio. All rights reserved.
+//
+
+#ifndef WeiBo_Bridge_h
+#define WeiBo_Bridge_h
+
+#import "CZAdditions.h"
+
+#endif /* WeiBo_Bridge_h */
